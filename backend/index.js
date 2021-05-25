119075178d75af52283b98c2c665798ce6e3bda3
daniel wang <daniel867181@gmail.com>
2021-05-25 20:53:56 +1000

    integrating mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import mongodb from 'mongodb';
+
+// configure environment variables
+dotenv.config()
+const MongoClient =  mongodb.MongoClient
+const port = process.env.PORT || 8000
+
+// Routes
+import entriesRoutes from './routes/entries.js';
 
 // Info for our server
 const app = express();
-const port = 3080
+
 
 // Middleware
 app.use(cors());
@@ -13,6 +24,9 @@ app.use(
       extended: true
     })
 )
+
+// main routes
+app.use('/entries', entriesRoutes);
 
 app.listen(port, () => {
     console.log(`Backend server running on port: ${port}`);
